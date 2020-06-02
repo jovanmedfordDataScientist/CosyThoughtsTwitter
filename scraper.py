dc96020e5d6b027daade985b0d9b7ded91ab71e1
Jovan Medford <jovan_medford@hotmail.com>
2020-06-01 20:56:46 -0400

    Feat: Download quotes to 'goodreads_'+ tag.html

--- scraper.py
+++ scraper.py
@@ -4,3 +4,12 @@ import pandas
 import numpy
 
 
+base_url = "https://www.goodreads.com/quotes/tag/"
+tags = ['life','love','success']
+
+for tag in tags:
+    response = requests.get(base_url + tag)
+    content = response.content
+    filename = 'goodreads_' + tag
+    with open(filename, mode = 'wb') as file:
+        file.write(response.content)
