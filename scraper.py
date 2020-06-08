b98bc5443971642788efaf3d13901ec3e990dfbb
Jovan Medford <jovan_medford@hotmail.com>
2020-06-08 10:45:40 -0400

    Feat: Add working example of scraping one quote from goodreads

--- scraper.py
+++ scraper.py
@@ -15,16 +15,19 @@ for tag in tags:`
     with open(filename, mode = 'wb') as file:
         file.write(response.content)
 '''
+
 def convert_html_to_soup(filename):
     '''Takes in an HTML file and turns it into soup with BeautifulSoup'''
     with open(filename, mode = 'rb') as file:
         soup = BeautifulSoup(file, features="html.parser")
     return soup
 
-quote_block = soup.findAll(attrs={"class": "quoteText"})[0]
+
+
+souped = convert_html_to_soup('goodreads_love.html')
+quote_block = souped.findAll(attrs={"class": "quoteText"})[0]
 quote = re.findall(r'“(.*?)”', quote_block.text)[0]
 dirty_author = quote_block.span.text
 author = dirty_author.replace('\n','').strip(' ')
-
 
 
